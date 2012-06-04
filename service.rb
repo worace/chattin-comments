d1813b3f94ad2008ce9be998bb6dfd60cb2a544a
Charles Strahan <charles.c.strahan@gmail.com>
2012-06-04 12:05:50 -0400

    Clean up service

--- service.rb
+++ service.rb
@@ -3,38 +3,17 @@ require 'active_record'
 require 'sinatra/base'
 
 require 'models/comment.rb'
+require 'helpers'
+require 'config'
 
 class Service < Sinatra::Base
-
-  module Helpers
-    private
-
-    def env
-      @env ||= ENV["RACK_ENV"] || ENV["SINATRA_ENV"] || "development"
-    end
-
-    def production?
-      @env == "production"
-    end
-
-    def development?
-      @env == "development"
-    end
-
-    def json_body
-      request.body.rewind
-      JSON.parse(request.body.read)
-    end
-  end
-
-  register Helpers
   helpers  Helpers
 
   configure do
     databases = YAML.load_file("config/database.yml")
-    ActiveRecord::Base.establish_connection(databases[env])
+    ActiveRecord::Base.establish_connection(databases[Config.env])
 
-    if development?
+    if Config.development?
       ActiveRecord::Base.logger = Logger.new(STDOUT)
     end
   end
@@ -45,9 +24,13 @@ class Service < Sinatra::Base
     content_type :json
   end
 
+  get '/comments/:id' do
+    id = params[:id].to_i
+    Comment.where(id: id).first.to_json
+  end
+
   get '/comments/presentation/:id' do
     presentation_id = params[:id].to_i
-
     Comment.where(presentation_id: presentation_id).to_json
   end
 
